7dab33c0058d474bead40810d96a67c680f64b03
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-01 17:10:19 +0100

    Enhance error handling and logging in personal_info route

--- app/routes/personal_info_routes.py
+++ app/routes/personal_info_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify, Response
+from flask import Blueprint, jsonify, Response, current_app
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from sqlalchemy.exc import NoResultFound, MultipleResultsFound, SQLAlchemyError
 
 from app.services.personal_info_service import fetch_personal_info
 
@@ -10,8 +11,18 @@ personal_info_bp = Blueprint('personal_info', __name__)
 @jwt_required()
 def get_personal_info() -> tuple[Response, int]:
     current_user = get_jwt_identity()
-    personal_info = fetch_personal_info(current_user)
-    if personal_info is None:
-        return jsonify({'error': 'No personal info found for username.'}), 404
 
-    return jsonify(personal_info), 200
+    def handle_error(exception, status_code):
+        error_message = {'error': str(exception)}
+        return jsonify(error_message), status_code
+
+    try:
+        personal_info = fetch_personal_info(current_user)
+        current_app.logger.info(f'Responded with personal info for {current_user}.')
+        return jsonify(personal_info), 200
+    except NoResultFound as ex:
+        return handle_error(ex, 404)
+    except MultipleResultsFound as ex:
+        return handle_error(ex, 409)
+    except SQLAlchemyError as ex:
+        return handle_error(ex, 500)
