611c229cccfcc3ad36495b6c8f23c3b84ff404f4
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-02 20:20:34 +0100

    Refactor and improve 'get_person_from_db' function

--- tests/repositories/tests_personal_info_repository.py
+++ tests/repositories/tests_personal_info_repository.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 
 import pytest
-from sqlalchemy.exc import SQLAlchemyError, NoResultFound, MultipleResultsFound
+from sqlalchemy.exc import SQLAlchemyError, NoResultFound
 
 from app.repositories.personal_info_repository import get_person_from_db
 
@@ -9,8 +9,8 @@ from app.repositories.personal_info_repository import get_person_from_db
 def test_get_person_from_db_success(app_with_client):
     app, _ = app_with_client
     with app.app_context():
-        person = get_person_from_db('test_user')
-        assert person.username == 'test_user'
+        person = get_person_from_db(1)
+        assert person.name == 'test'
         assert person.surname == 'tester'
 
 
@@ -18,16 +18,8 @@ def test_get_person_from_db_no_result(app_with_client):
     app, _ = app_with_client
     with app.app_context():
         with pytest.raises(NoResultFound) as e:
-            get_person_from_db('non_existing_user')
-        assert str(e.value) == 'No user found with username non_existing_user.'
-
-
-def test_get_person_from_db_multiple_results(app_with_client):
-    app, _ = app_with_client
-    with app.app_context():
-        with pytest.raises(MultipleResultsFound) as e:
-            get_person_from_db('duplicate_user')
-        assert str(e.value) == 'Multiple users found with username duplicate_user.'
+            get_person_from_db(2)
+        assert str(e.value) == 'USER NOT FOUND: 2.'
 
 
 def test_get_person_from_db_sqlalchemy_error(app_with_client):
@@ -37,5 +29,5 @@ def test_get_person_from_db_sqlalchemy_error(app_with_client):
             mock_query.filter_by.side_effect = SQLAlchemyError(
                 "A database error occurred")
             with pytest.raises(SQLAlchemyError) as e:
-                get_person_from_db('test_user')
-            assert str(e.value) == 'Error retrieving user from the database.'
+                get_person_from_db(1)
+            assert str(e.value) == 'DATABASE CONNECTION ERROR.'
