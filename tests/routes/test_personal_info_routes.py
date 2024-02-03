4a4d37f244762a413e3ffb2ea97199cc6fff9612
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-03 22:49:15 +0100

    Reformat to be in line with PEP8.

--- tests/routes/test_personal_info_routes.py
+++ tests/routes/test_personal_info_routes.py
@@ -13,8 +13,9 @@ def test_get_personal_info_success(app_with_client):
 	setup_test_user_in_db(app)
 	token = generate_token_for_user_id_1(app)
 
-	response = test_client.get('/applicant/personal_info/',
-							   headers={'Authorization': f'Bearer {token}'})
+	response = test_client.get(
+			'/applicant/personal_info/',
+			headers={'Authorization': f'Bearer {token}'})
 	assert response.status_code == 200
 	assert response.json['name'] == 'test'
 	assert response.json['surname'] == 'tester'
@@ -26,8 +27,9 @@ def test_get_personal_info_no_result(app_with_client):
 	remove_test_user_from_db(app)
 	token = generate_token_for_user_id_1(app)
 
-	response = test_client.get('/applicant/personal_info/',
-							   headers={'Authorization': f'Bearer {token}'})
+	response = test_client.get(
+			'/applicant/personal_info/',
+			headers={'Authorization': f'Bearer {token}'})
 	assert response.status_code == 404
 	assert response.json['error'] == 'USER_NOT_FOUND'
 	assert response.json['details'] == 'User not found with id: 1'
@@ -41,9 +43,9 @@ def test_get_personal_info_sqlalchemy_error(app_with_client):
 			'app.routes.personal_info_routes.fetch_personal_info') as mock_fetch:
 		mock_fetch.side_effect = SQLAlchemyError("A database error occurred")
 
-		response = test_client.get('/applicant/personal_info/',
-								   headers={
-									   'Authorization': f'Bearer {token}'})
+		response = test_client.get(
+				'/applicant/personal_info/',
+				headers={'Authorization': f'Bearer {token}'})
 
 		assert response.status_code == 500
 		assert response.json['error'] == 'COULD_NOT_FETCH_USER'
