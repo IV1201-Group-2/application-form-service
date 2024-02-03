c27b3c9fa24c5963b438097b63be8bb90cc6a216
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-04 00:42:54 +0100

    Add unit tests for 'fetch_competences' function

---
+++ tests/services/test_competences_service.py
@@ -0,0 +1,42 @@
+from unittest.mock import patch
+
+import pytest
+from sqlalchemy.exc import NoResultFound, SQLAlchemyError
+
+from app.services.competences_service import fetch_competences
+from tests.utilities.test_utilities import remove_competences_from_db, \
+    setup_competences_in_db
+
+
+def test_fetch_competences_success(app_with_client):
+    app, _ = app_with_client
+    setup_competences_in_db(app)
+    with app.app_context():
+        result = fetch_competences()
+        assert result[1] == 'tester'
+        assert result[2] == 'developer'
+
+
+def test_fetch_competences_no_result(app_with_client):
+    app, _ = app_with_client
+    remove_competences_from_db(app)
+    with app.app_context():
+        with pytest.raises(NoResultFound) as exception_info:
+            fetch_competences()
+
+        exception = exception_info.value
+        assert isinstance(exception, NoResultFound)
+
+
+def test_fetch_competences_sqlalchemy_error(app_with_client):
+    app, _ = app_with_client
+
+    with patch(
+            'app.services.competences_service'
+            '.get_competences_from_db') as mock_fetch:
+        mock_fetch.side_effect = SQLAlchemyError("DATABASE CONNECTION ERROR.")
+
+        with app.app_context():
+            with pytest.raises(SQLAlchemyError) as exception:
+                fetch_competences()
+            assert str(exception.value) == 'DATABASE CONNECTION ERROR.'
