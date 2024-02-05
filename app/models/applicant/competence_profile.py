1a748768442c9f0d9272427b60f2108c2561134a
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-06 00:02:04 +0100

    Adjust to be in line with PEP8

--- app/models/applicant/competence_profile.py
+++ app/models/applicant/competence_profile.py
@@ -6,9 +6,10 @@ class CompetenceProfile(database.Model):  # type: ignore
     Represents a competence profile in the database.
 
     :ivar competence_profile_id: The unique ID of the competence profile.
-    :ivar person_id: The ID of the person associated with this competence profile.
+    :ivar person_id: The ID of the person associated with this profile.
     :ivar competence_id: The ID of the competence associated with this profile.
-    :ivar years_of_experience: The number of years of experience in the given competence.
+    :ivar years_of_experience: The number of years of experience in the given
+          competence.
     """
 
     __tablename__ = 'competence_profile'
@@ -23,9 +24,10 @@ class CompetenceProfile(database.Model):  # type: ignore
         """
         Initializes a new CompetenceProfile object.
 
-        :param person_id: The ID of the person associated with this competence profile.
-        :param competence_id: The ID of the competence associated with this profile.
-        :param years_of_experience: The number of years of experience in the given competence.
+        :param person_id: The ID of the person associated with this profile.
+        :param competence_id: The ID of the competence associated with profile.
+        :param years_of_experience: The number of years of experience in the
+               given competence.
         """
         self.person_id = person_id
         self.competence_id = competence_id
