5ab5a194e7adfb00afb71a9f74cda9fc75058110
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-06 17:25:37 +0100

    Improve exception logging.

--- app/repositories/applicant/personal_info_repository.py
+++ app/repositories/applicant/personal_info_repository.py
@@ -1,5 +1,3 @@
-from typing import NoReturn
-
 from flask import current_app
 from sqlalchemy.exc import NoResultFound, SQLAlchemyError
 
@@ -16,20 +14,9 @@ def get_person_from_db(user_id: int) -> Person:
 
     try:
         return Person.query.filter_by(person_id=user_id).one()
-    except NoResultFound:
-        __log_and_raise(NoResultFound, f'USER NOT FOUND: {user_id}.')
-    except SQLAlchemyError:
-        __log_and_raise(SQLAlchemyError, 'DATABASE CONNECTION ERROR.')
-
-
-def __log_and_raise(exception_type: type, message: str) -> NoReturn:
-    """
-    Logs an error and raises an exception.
-
-    :param exception_type: The type of the exception.
-    :param message: The error message.
-    """
-
-    exception = exception_type(message)
-    current_app.logger.error(exception)
-    raise exception
+    except NoResultFound as exception:
+        current_app.logger.error(exception)
+        raise NoResultFound(f'USER NOT FOUND: {user_id}.')
+    except SQLAlchemyError as exception:
+        current_app.logger.error(exception)
+        raise SQLAlchemyError('COULD NOT FETCH COMPETENCES.')
