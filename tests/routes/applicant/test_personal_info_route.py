789c3b296735982a4d236fefb771b212435ac2c0
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-14 19:29:33 +0100

    Refactor function name.

--- tests/routes/applicant/test_personal_info_route.py
+++ tests/routes/applicant/test_personal_info_route.py
@@ -3,24 +3,24 @@ from unittest.mock import patch
 from sqlalchemy.exc import SQLAlchemyError
 
 from tests.utilities.test_utilities import generate_token_for_user_id_1, \
-    remove_test_user_from_db, \
-    setup_test_user_in_db
+    remove_test_user_1_from_db, \
+    setup_test_user_1_in_db
 
 
 def test_get_personal_info_success(app_with_client):
     app, test_client = app_with_client
-    setup_test_user_in_db(app)
+    setup_test_user_1_in_db(app)
     token = generate_token_for_user_id_1(app)
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
-            headers={'Authorization': f'Bearer {token}'})
+        '/api/application-form/applicant/personal-info/',
+        headers={'Authorization': f'Bearer {token}'})
     assert response.status_code == 200
     assert response.json['name'] == 'test'
     assert response.json['surname'] == 'tester'
     assert response.json['role'] == 2
 
-    remove_test_user_from_db(app)
+    remove_test_user_1_from_db(app)
 
 
 def test_get_personal_info_no_result(app_with_client):
@@ -28,8 +28,8 @@ def test_get_personal_info_no_result(app_with_client):
     token = generate_token_for_user_id_1(app)
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
-            headers={'Authorization': f'Bearer {token}'})
+        '/api/application-form/applicant/personal-info/',
+        headers={'Authorization': f'Bearer {token}'})
     assert response.status_code == 404
     assert response.json['error'] == 'USER_NOT_FOUND'
     assert response.json['details'] == 'User not found with id: 1'
@@ -43,8 +43,8 @@ def test_get_personal_info_sqlalchemy_error(mock_fetch, app_with_client):
     mock_fetch.side_effect = SQLAlchemyError("A database error occurred")
 
     response = test_client.get(
-            '/api/application-form/applicant/personal-info/',
-            headers={'Authorization': f'Bearer {token}'})
+        '/api/application-form/applicant/personal-info/',
+        headers={'Authorization': f'Bearer {token}'})
 
     assert response.status_code == 500
     assert response.json['error'] == 'COULD_NOT_FETCH_USER'
