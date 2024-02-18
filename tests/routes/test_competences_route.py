22c77065974600d07e0ce444a4bc592561a4789d
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-18 13:23:17 +0100

    Refactor test cases for competences endpoint

--- tests/routes/test_competences_route.py
+++ tests/routes/test_competences_route.py
@@ -2,40 +2,40 @@ from unittest.mock import patch
 
 from sqlalchemy.exc import SQLAlchemyError
 
-from tests.utilities.test_utilities import generate_token_for_user_id_1, \
+from tests.utilities.test_utilities import generate_token_for_person_id_1, \
     remove_competences_from_db, setup_competences_in_db
 
 
 def test_get_competences_success(app_with_client):
     app, test_client = app_with_client
     setup_competences_in_db(app)
-    token = generate_token_for_user_id_1(app)
+    token = generate_token_for_person_id_1(app)
 
     response = test_client.get(
             '/api/application-form/competences/',
             headers={'Authorization': f'Bearer {token}'})
     assert response.status_code == 200
-    assert response.json == {'1': 'tester', '2': 'developer'}
+    assert response.json == [{'competence_id': 1, 'name': 'tester'},
+                             {'competence_id': 2, 'name': 'developer'}]
 
     remove_competences_from_db(app)
 
 
 def test_competences_no_result(app_with_client):
     app, test_client = app_with_client
-    token = generate_token_for_user_id_1(app)
+    token = generate_token_for_person_id_1(app)
 
     response = test_client.get(
             '/api/application-form/competences/',
             headers={'Authorization': f'Bearer {token}'})
     assert response.status_code == 404
     assert response.json['error'] == 'COMPETENCES_NOT_FOUND'
-    assert response.json['details'] == 'No competences found in the database'
 
 
-@patch('app.routes.competences_routes.fetch_competences')
+@patch('app.routes.competences_route.fetch_competences')
 def test_get_personal_info_sqlalchemy_error(mock_fetch, app_with_client):
     app, test_client = app_with_client
-    token = generate_token_for_user_id_1(app)
+    token = generate_token_for_person_id_1(app)
 
     mock_fetch.side_effect = SQLAlchemyError("A database error occurred")
 
@@ -45,5 +45,3 @@ def test_get_personal_info_sqlalchemy_error(mock_fetch, app_with_client):
 
     assert response.status_code == 500
     assert response.json['error'] == 'COULD_NOT_FETCH_COMPETENCES'
-    assert response.json['details'] == ('Could not '
-                                        'fetch competences from database')
