5b58d582052c14048482ec712db7081866196e08
Azmeer K. <azmeer.khawaja@outlook.com>
2024-02-18 12:06:39 +0100

    Improve documentation. Remove unused exception messages.

--- app/repositories/competences_repository.py
+++ app/repositories/competences_repository.py
@@ -6,18 +6,24 @@ from app.models.competence import Competence
 
 def get_competences_from_db() -> list[Competence]:
     """
-    Retrieves a list of competences from the database.
+    Retrieve a list of competences from the database.
 
-    :return: A list of Competence objects.
+    This function retrieves a list of competences from the database. If the
+    retrieval fails, it raises an SQLAlchemyError. If no competences are found,
+    it raises a NoResultFound exception.
+
+    :returns: A list of Competence objects.
+    :raises SQLAlchemyError: If there is an issue with the database operation.
+    :raises NoResultFound: If no competences are found in the database.
     """
 
     try:
         competences = Competence.query.all()
     except SQLAlchemyError as exception:
         current_app.logger.error(exception)
-        raise SQLAlchemyError('COULD NOT FETCH COMPETENCES') from exception
+        raise SQLAlchemyError
 
     if not competences:
         current_app.logger.error(NoResultFound('NO COMPETENCES FOUND'))
-        raise NoResultFound('NO COMPETENCES FOUND')
+        raise NoResultFound
     return competences
